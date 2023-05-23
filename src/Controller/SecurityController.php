2ba12afc86104c6dea5034e47aa018aff6c368c1
ajrockr <anjorizz@gmail.com>
2023-05-23 10:21:37 -0400

    Login form tweaks
    Added microsoft/google login checks. Customized login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\SiteConfig;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, EntityManagerInterface $entityManager): Response
     {
+        $microsoftEnabled = $entityManager->getRepository(SiteConfig::class)->findOneBy(['configName' => 'auth_useMicrosoft'])->getConfigValue();
+        $googleEnabled = $entityManager->getRepository(SiteConfig::class)->findOneBy(['configName' => 'auth_useGoogle'])->getConfigValue();
+
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
@@ -21,7 +26,12 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+             'error' => $error,
+             'googleEnabled' => $googleEnabled,
+             'microsoftEnabled' => $microsoftEnabled
+            ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
