6f0800e69200b76f71b90f5a802ec213949ee421
ajrockr <anjorizz@gmail.com>
2023-05-24 15:11:29 -0400

    little bit of user profile styling

--- src/Form/UserProfileType.php
+++ src/Form/UserProfileType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,15 +15,38 @@ class UserProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('location')
-            ->add('department')
-            ->add('phone')
-            ->add('extension')
-            ->add('title')
-            ->add('homepage')
-            ->add('manager')
+            ->add('location', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control select2'
+                ]
+            ])
+            ->add('department', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('extension', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('homepage', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('save', SubmitType::class, [
-                'attr' => ['class' => 'save']
+                'attr' => ['class' => 'btn btn-block btn-primary mt-3']
             ])
         ;
     }
