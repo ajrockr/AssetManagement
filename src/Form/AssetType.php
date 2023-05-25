389b9f05ff780d6f687a9142d4a8ae73f58e891c
ajrockr <anjorizz@gmail.com>
2023-05-25 08:37:28 -0400

    asset/edit work
    Still need to remove the time from the DateTime fields. group them. might have to manually render the from inside the twig template

--- src/Form/AssetType.php
+++ src/Form/AssetType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Asset;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,76 @@ class AssetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('serialnumber')
-            ->add('assettag')
-            ->add('purchasedate')
-            ->add('purchasedfrom')
-            ->add('warrantystartdate')
-            ->add('warrantyenddate')
-            ->add('condition')
-            ->add('make')
-            ->add('model')
-            ->add('decomisioned')
+            ->add('serialnumber', TextType::class, [
+                'label' => 'Serial Number',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('assettag', TextType::class, [
+                'label' => 'Asset Tag',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('purchasedate', DateTimeType::class, [
+                'label' => 'Purchased Date',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('purchasedfrom', TextType::class, [
+                'label' => 'Purchased From',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('warrantystartdate', DateTimeType::class, [
+                'label' => 'Warranty Start',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('warrantyenddate', DateTimeType::class, [
+                'label' => 'Warranty End',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('condition', TextType::class, [
+                'label' => 'Condition',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('make', TextType::class, [
+                'label' => 'Make',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('model', TextType::class, [
+                'label' => 'Model',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('decomisioned', CheckboxType::class, [
+                'label' => 'Decommissioned',
+                'row_attr' => [
+                    'class' => 'form-check-inline'
+                ],
+                'required' => false,
+                'label_attr' => [
+                    'class' => 'form-check-label mr-2 my-2'
+                ],
+                'attr' => [
+                    'class' => 'form-check-input'
+                ]
+            ])
         ;
     }
 
