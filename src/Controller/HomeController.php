9b0a7a3febb557ee70e0a12404236027377f5355
ajrockr <anjorizz@gmail.com>
2023-06-03 17:30:06 -0400

    customizing front page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Repository\AssetCollectionRepository;
+use App\Repository\AssetRepository;
+use App\Repository\AssetStorageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly AssetRepository $assetRepository,
+        private readonly AssetStorageRepository $assetStorageRepository,
+        private readonly AssetCollectionRepository $assetCollectionRepository
+    ) {}
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'totalAssetsCount' => $this->getTotalAssetsCount(),
+            'totalCollectedAssetsCount' => $this->getCollectedAssetsCount(),
+            'totalDecommissionedAssetsCount' => $this->getDecommissionedAssetsCount(),
+            'totalStoragesCount' => $this->getStorageCount(),
+            'storageSizes' => $this->getStorageSizes()
         ]);
+    }
+
+    private function getStorageCount(): int
+    {
+        return count($this->assetStorageRepository->findAll());
+    }
+
+    private function getStorageSizes(): array
+    {
+        $storages = $this->assetStorageRepository->findAll();
+        $returnArray = [];
+        foreach ($storages as $storage) {
+            $returnArray[$storage->getName()] = sizeof($storage->getStorageData(), 1);
+        }
+
+        return $returnArray;
+    }
+
+    private function getTotalAssetsCount(): int
+    {
+        return count($this->assetRepository->findAll());
+    }
+
+    private function getCollectedAssetsCount(): int
+    {
+        return count($this->assetCollectionRepository->findAll());
+    }
+
+    private function getDecommissionedAssetsCount(): int
+    {
+        return count($this->assetRepository->findBy(['decomisioned' => true]));
     }
 }
