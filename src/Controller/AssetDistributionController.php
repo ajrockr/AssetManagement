b55c3808ae733cf907ee1e545bfabfd04334b24b
ajrockr <anjorizz@gmail.com>
2023-06-05 11:56:59 -0400

    distribution revoceation implemented

--- src/Controller/AssetDistributionController.php
+++ src/Controller/AssetDistributionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\AssetCollection;
 use App\Entity\AssetDistribution;
 use App\Repository\AssetCollectionRepository;
 use App\Repository\AssetDistributionRepository;
@@ -66,5 +67,110 @@ class AssetDistributionController extends AbstractController
         $this->addFlash('success', 'Sent asset for distribution.');
 
         return $this->redirectToRoute('app_asset_distribution');
+    }
+
+    #[Route('/asset/distribution/roulette', name: 'app_asset_distribution_roulette')]
+    public function roulette(AssetDistributionRepository $assetDistributionRepository, AssetRepository $assetRepository, UserRepository $userRepository): Response
+    {
+        $returnArray = [];
+        foreach ($assetDistributionRepository->getDistributions() as $asset) {
+            $assetInfo = $assetRepository->findOneBy(['id' => $asset->getDeviceId()]);
+            $userInfo = $userRepository->findOneBy(['id' => $asset->getUserId()]);
+            $returnArray[] = [
+                'id' => $asset->getId(),
+                'assettag' => !($assetInfo) ? 'null' : $assetInfo->getAssettag(),
+                'serialnumber' => !($assetInfo) ? 'null' : $assetInfo->getSerialnumber(),
+                'userFirstName' => !($userInfo) ? 'null' : $userInfo->getFirstname(),
+                'userSurname' => !($userInfo) ? 'null' : $userInfo->getSurname(),
+                'location' => ($asset->getLocation()) ?: null,
+                'createdAt' => $asset->getCreatedAt()
+            ];
+        }
+
+        return $this->render('asset_distribution/roulette.html.twig', [
+            'assets' => $returnArray
+        ]);
+    }
+
+    #[Route('/asset/distribution/clear/{id}', name: 'app_distribute_clear')]
+    public function clear(AssetDistributionRepository $assetDistributionRepository, AssetCollectionRepository $assetCollectionRepository, int $id): Response
+    {
+        if (gettype($id) !== "integer") {
+            throw new \Exception("Uh uh uh...");
+        }
+
+        $asset = $assetDistributionRepository->findOneBy(['id' => $id]);
+        $assetId = $asset->getDeviceId();
+
+        if ($assetCollection = $assetCollectionRepository->findOneBy(['DeviceID' => $assetId])) {
+            $assetCollectionRepository->remove($assetCollection, true);
+        }
+
+        $asset->setDistributedBy($this->getUser()->getId());
+        $asset->setDistributedAt(new \DateTimeImmutable('now'));
+        $assetDistributionRepository->save($asset, true);
+
+        return $this->redirectToRoute('app_asset_distribution_roulette');
+    }
+
+    #[Route('/asset/distribution/revoke', name: 'app_distribute_revoke')]
+    public function revoke(AssetRepository $assetRepository, UserRepository $userRepository, AssetDistributionRepository $assetDistributionRepository): Response
+    {
+        $distributions = $assetDistributionRepository->getAllDistributions();
+        $returnArray = [];
+
+        foreach ($distributions as $distribution) {
+            $assetInfo = $assetRepository->findOneBy(['id' => $distribution->getDeviceId()]);
+            $userInfo = $userRepository->findOneBy(['id' => $distribution->getUserId()]);
+            if (null === $distUserInfo = $userRepository->findOneBy(['id' => $distribution->getDistributedBy()])) {
+                $distUserInfo = $userRepository->findOneBy(['id' => $distribution->getDistributionSetBy()]);
+            }
+
+            $returnArray[] = [
+                'id' => $distribution->getId(),
+                'assetTag' => $assetInfo->getAssettag(),
+                'serialNumber' => $assetInfo->getSerialnumber(),
+                'location' => $distribution->getLocation(),
+                'distributedAt' => $distribution->getDistributedAt(),
+                'distributedByFirstName' => $distUserInfo->getFirstname(),
+                'distributedBySurname' => $distUserInfo->getSurname(),
+                'userFirstName' => $userInfo->getFirstname(),
+                'userSurname' => $userInfo->getSurname(),
+                'notes' => $distribution->getNotes()
+            ];
+        }
+
+        return $this->render('asset_distribution/revoke.html.twig', [
+            'assets' => $returnArray
+        ]);
+    }
+
+    #[Route('/asset/distribution/revokedist', name: 'app_distribute_revoke_id')]
+    public function revokeAsset(Request $request, AssetDistributionRepository $assetDistributionRepository, AssetCollectionRepository $assetCollectionRepository): Response
+    {
+        $id = $request->request->get('distributionId');
+
+        $assetDistribution = $assetDistributionRepository->findOneBy(['id' => $id]);
+        $assetCollection = $assetCollectionRepository->findOneBy(['DeviceID' => $assetDistribution->getDeviceId()]);
+
+        $assetDistribution->setDistributedAt(null);
+        $assetDistribution->setDistributedBy(null);
+
+        // If the collection item was removed, add it again
+        if (null === $assetCollection) {
+            $assetCollection = new AssetCollection;
+            $assetCollection->setCollectedBy($this->getUser()->getId());
+            $assetCollection->setDeviceID($assetDistribution->getDeviceId());
+            $assetCollection->setCollectionLocation($assetDistribution->getLocation());
+            $assetCollection->setCollectedFrom($assetDistribution->getUserId());
+            $assetCollection->setCollectedDate($assetDistribution->getCreatedAt());
+            $assetCollection->setCheckedout(false);
+            $assetCollectionRepository->save($assetCollection, true);
+        }
+
+        $assetDistributionRepository->remove($assetDistribution, true);
+
+        $this->addFlash('success', 'Revoked asset from distribution.');
+        return $this->redirectToRoute('app_distribute_revoke');
     }
 }
--- src/Repository/AssetDistributionRepository.php
+++ src/Repository/AssetDistributionRepository.php
@@ -39,28 +39,23 @@ class AssetDistributionRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return AssetDistribution[] Returns an array of AssetDistribution objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?AssetDistribution
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function getDistributions()
+    {
+        return $this->createQueryBuilder('d')
+            ->select('d')
+            ->orderBy('d.createdAt', 'ASC')
+            ->where('d.distributedBy is NULL')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    public function getAllDistributions()
+    {
+        return $this->createQueryBuilder('d')
+            ->select('d')
+            ->orderBy('d.createdAt', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
