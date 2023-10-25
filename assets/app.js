01f88355f47521e04016c6e2a18b09ae3ecc5a30
ajrockr <anjorizz@gmail.com>
2023-10-25 11:00:05 -0400

    big css changes to layout
    still wip

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,5 @@
 // /assets/app.js
 
-// import './styles/app.css';
-// import './styles/asset_collection.css';
-
 // import the main style sheet
 import './styles/main.scss';
 
@@ -15,7 +12,6 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-
 // start the Stimulus application
 import './bootstrap';
 
@@ -26,11 +22,12 @@ const $ = require('jquery');
 global.$ = global.jQuery = $
 
 // https://stackoverflow.com/questions/63082529/how-to-properly-introduce-a-light-dark-mode-in-bootstrap
-document.getElementById('btnSwitch').addEventListener('click',()=>{
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
-    }
-    else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
-    }
-})
+// document.getElementById('btnSwitch').addEventListener('click',()=>{
+//     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+//         document.documentElement.setAttribute('data-bs-theme','light')
+//     }
+//     else {
+//         document.documentElement.setAttribute('data-bs-theme','dark')
+//     }
+// })
+
