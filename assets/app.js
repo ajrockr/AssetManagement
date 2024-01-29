43e2c390b2e6d8a8b38dcb6d744d79bb017747c8
ajrockr <anjorizz@gmail.com>
2024-01-29 12:26:10 -0500

    updated symfony stimulus bundle

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+<<<<<<< ours
 // /assets/app.js
 
 // import the main style sheet
@@ -29,3 +30,37 @@ document.addEventListener('chartjs:init', function (event) {
     Chart.register(zoomPlugin);
     Chart.register(Colors);
 });
+=======
+import './bootstrap.js';
+// /assets/app.js
+
+// import the main style sheet
+import './styles/main.scss';
+
+// import bootstrap js
+import 'bootstrap';
+
+// fontawesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+// start the Stimulus application
+import './bootstrap';
+
+// require jQuery
+const $ = require('jquery');
+
+// create jQuery global
+global.$ = global.jQuery = $
+
+import zoomPlugin from 'chartjs-plugin-zoom';
+import { Colors } from 'chart.js';
+
+document.addEventListener('chartjs:init', function (event) {
+    const Chart = event.detail.Chart;
+    Chart.register(zoomPlugin);
+    Chart.register(Colors);
+});
+>>>>>>> theirs
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,3 +1,4 @@
+<<<<<<< ours
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
@@ -9,3 +10,24 @@ export const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+=======
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.[jt]sx?$/
+));
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.[jt]sx?$/
+));
+
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
+>>>>>>> theirs
