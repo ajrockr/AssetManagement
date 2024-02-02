1a1edd5ccbe97ded7a0eb2e9ff5fc3417313b492
ajrockr <anjorizz@gmail.com>
2024-02-02 12:56:55 -0500

    refactor(importuser): Broke up responsibilities into methods.

--- src/Controller/Admin/ImportUserController.php
+++ src/Controller/Admin/ImportUserController.php
@@ -4,11 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Form\ImportUserType;
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ImportUserController extends AbstractController
@@ -21,8 +21,9 @@ class ImportUserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->get('fileCsv')->getData();
-            
+
             $this->processUpload($entityManager, $data);
+            // todo 1) Process upload 2) present user with data and column mappings 3) commit to database
         }
 
         return $this->render('admin/import_user/index.html.twig', [
@@ -30,52 +31,67 @@ class ImportUserController extends AbstractController
         ]);
     }
 
-    /**
-     * @throws \Exception
-     */
     private function processUpload(EntityManagerInterface $entityManager, $file): void
     {
-        if (($handle = fopen($file->getPathname(), "r")) !== false) {
-            $userRepository = $entityManager->getRepository(User::class);
-            try {
-                while (($data = fgetcsv($handle)) !== false) {
-                    // TODO: Eventually support files that have headers.
-                    // Skip header, we know $data[0] is the Description
-                    if (($data[0] == "Description")) {
-                        continue;
-                    }
-
-                    // Bypass the unique constraint by skipping the record
-                    if ($userRepository->findOneBy(['username' => $data[1]])) {
-                        continue;
-                    }
-
-                    // We need an email field, skip if no email in record (bad import data)
-                    if (empty($data[2])) {
-                        continue;
-		    }
-
-		    if (! isset($data[6]) || empty($data[6])) {
-		        $type = 'Student';
-		    }
-
-                    $user = new User();
-                    $user->setSurname($data[4]);
-                    $user->setFirstname($data[3]);
-                    $user->setUsername($data[1]);
-                    $user->setEmail($data[2]);
-                    $user->setTitle($data[0]);
-                    $user->setRoles(['ROLE_DENY_LOGIN']);
-                    $user->setEnabled(false);
-                    $user->setUserUniqueId($data[5]);
-                    $user->setType($type);
-                    $entityManager->persist($user);
+        $userData = [];
+        if (($handle = fopen($file->getPathname(), 'r')) !== false) {
+            while (($data = fgetcsv($handle)) !== false) {
+                // TODO: Eventually support files that have headers.
+                // Skip header, we know $data[0] is the Description
+                if (($data[0] == 'Description')) {
+                    continue;
                 }
 
-                $entityManager->flush();
-            } catch (UniqueConstraintViolationException $e) {
-                throw new \Exception($e->getMessage());
+                $type = (empty($data[6])) ? 'Student' : $data[6];
+                $userData[] = [
+                    'title' => $data[0],
+                    'surname' => $data[4],
+                    'firstname' => $data[3],
+                    'email' => $data[2],
+                    'username' => $data[1],
+                    'uniqueId' => $data[5],
+                    'type' => $type,
+                ];
             }
         }
+
+        $this->commitToDatabase($entityManager, $userData);
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param array $userData
+     * @return void
+     */
+    private function commitToDatabase(EntityManagerInterface $entityManager, array $userData): void
+    {
+        $userRepository = $entityManager->getRepository(User::class);
+        try {
+            foreach ($userData as $user) {
+                // Skip if username already exists
+                if ($userRepository->findOneBy(['username' => $user['username']])) {
+                    $this->addFlash('warning', sprintf('User %s was skipped due to username conflicts.', $user['username']));
+                    continue;
+                }
+
+                $userEntity = new User();
+                $userEntity->setSurname($user['surname']);
+                $userEntity->setFirstname($user['firstname']);
+                $userEntity->setUsername($user['username']);
+                $userEntity->setEmail($user['email']);
+                $userEntity->setTitle($user['title']);
+                $userEntity->setRoles(['ROLE_DENY_LOGIN']);
+                $userEntity->setEnabled(false);
+                $userEntity->setUserUniqueId($user['uniqueId']);
+                $userEntity->setType($user['type']);
+                $entityManager->persist($userEntity);
+            }
+
+            $entityManager->flush();
+        } catch (ORMException $e) {
+            $this->addFlash('error', 'Failed to import users.');
+        }
+
+        $this->addFlash('success', 'Imported users.');
     }
 }
