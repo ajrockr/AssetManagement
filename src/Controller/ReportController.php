ee60a9c242463e7176aa99a53d00170954babe5e
ajrockr <anjorizz@gmail.com>
2024-02-06 10:40:52 -0500

    added report_viewer to hierarchy

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -9,9 +9,11 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/report')]
+#[IsGranted('ROLE_REPORT_VIEWER')]
 class ReportController extends AbstractController
 {
     #[Route('/', name: 'app_report_index')]
